fad283ae6404761beb755658e30ad8ffbe9c5e6b
Pedro Augusto <pedroaugennes@gmail.com>
2022-09-02 18:38:50 +0100

    media querie for < 668px
    Little menu btn

--- script.js
+++ script.js
@@ -4,10 +4,30 @@ function changeBackground() {
 
     if (scrollValue < 1) {
         navBar.classList.remove("bgchange")
+        menuBtn.classList.remove("bgchange")
     } else {
         navBar.classList.add("bgchange")
     }
 
 }
 
+function changeMenuSquare() {
+    var line1 = document.getElementById("line1")
+    var line2 = document.getElementById("line2")
+    var line3 = document.getElementById("line3")
+    
+    if (click == 0) {
+        line1.classList.add("liney")
+        line2.classList.add("linez")
+        line3.classList.add("linex")
+        click = 1
+    } else if (click != 0) {
+        line1.classList.remove("liney")
+        line2.classList.remove("linez")
+        line3.classList.remove("linex")
+        click = 0
+    }
+}
+
+var click = 0
 window.addEventListener('scroll', changeBackground)
