78cb1c1771123c063889c50cbf29bec866134174
Pedro Augusto <pedroaugennes@gmail.com>
2022-09-03 19:53:21 +0100

    responsivity home page

--- scripts/script.js
+++ scripts/script.js
@@ -15,16 +15,25 @@ function changeMenuSquare() {
     var line1 = document.getElementById("line1")
     var line2 = document.getElementById("line2")
     var line3 = document.getElementById("line3")
+    var slidemenu = document.getElementById("slideMenu")
+    var navBar = document.getElementById("header") // header opacity when no scroll
+    var opacity = document.getElementById("opacity")
     
     if (click == 0) {
         line1.classList.add("liney")
         line2.classList.add("linez")
         line3.classList.add("linex")
+        slidemenu.classList.add("open")
+        navBar.classList.add("bgchange")
+        //opacity.classList.add("openopa")
         click = 1
     } else if (click != 0) {
         line1.classList.remove("liney")
         line2.classList.remove("linez")
         line3.classList.remove("linex")
+        slidemenu.classList.remove("open")
+        navBar.classList.remove("bgchange")
+        //opacity.classList.remove("openopa")
         click = 0
     }
 }
