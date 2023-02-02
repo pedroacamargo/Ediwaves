aae1e15915e1c3b1f13e59841775231f91d84120
pedroacamargo <pedroaugennes@gmail.com>
2023-02-02 01:21:28 +0000

    Done ✅

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,13 +2,11 @@ var canvas = document.getElementById('canvas');
 var canvas2 = document.getElementById("canvas2")
 var canvas3 = document.getElementById("canvas3")
 var canvas4 = document.getElementById("canvas4")
-var canvas5 = document.getElementById("canvas5")
 var canvas6 = document.getElementById("canvas6")
 var c = canvas.getContext('2d'); 
 var c2 = canvas2.getContext('2d')
 var c3 = canvas3.getContext('2d')
 var c4 = canvas4.getContext('2d')
-var c5 = canvas5.getContext('2d')
 var c6 = canvas6.getContext('2d')
 var grad = c.createLinearGradient(0,0,0,169);
 grad.addColorStop(.1, "black");
@@ -17,7 +15,6 @@ c.fillStyle = grad;
 c2.fillStyle = "black"
 c3.fillStyle = "black"
 c4.fillStyle = "#0477BF"
-c5.fillStyle = "black"
 c6.fillStyle = "black"
 c.beginPath();
 c.moveTo(0,0)
@@ -55,15 +52,6 @@ c4.lineTo(1090, 200)
 c4.closePath()
 c4.fill()
 
-c5.beginPath();
-c5.moveTo(0,0)
-c5.lineTo(1090,0)
-c5.bezierCurveTo(960, 200, 850, 70, 800, 90)
-c5.bezierCurveTo(600, 200, 650, 100, 500, 50)
-c5.bezierCurveTo(400, 40, 350, 150, 200, 150)
-c5.bezierCurveTo(70, 150, 50, 20, 0, 70)
-c5.closePath();
-c5.fill()
 
 c6.beginPath();
 c6.moveTo(-100, 200)
